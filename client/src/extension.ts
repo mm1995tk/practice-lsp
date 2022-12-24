a84f1c01bb7e443eea123c8a2a2891b91f73166d
mm1995tk <jimi.woodstock1995@gmail.com>
2022-12-25 05:05:47 +0900

    整形

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,61 +1,60 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
-import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
-
+import * as path from "path";
+import { workspace, ExtensionContext } from "vscode";
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient/node';
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	const serverModule = context.asAbsolutePath(
-		path.join('~/.local/bin', 'server-exe')
-	);
+export const activate = (context: ExtensionContext) => {
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "languageServerExample",
+    "Language Server Example",
+    serverOptions({
+      serverModule: context.asAbsolutePath(
+        path.join("~/.local/bin", "server-exe")
+      ),
+    }),
+    clientOptions
+  );
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-		}
-	};
+  // Start the client. This will also launch the server
+  client.start();
+};
 
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
+export const deactivate = (): Thenable<void> | undefined => {
+  if (client) {
+    return client.stop();
+  }
+};
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
+// If the extension is launched in debug mode then the debug server options are used
+// Otherwise the run options are used
+const serverOptions: (deps: {
+  /**
+   * The server is implemented in node
+   */
+  serverModule: string;
+}) => ServerOptions = ({ serverModule }) => {
+  return {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+    },
+  };
+};
 
-	// Start the client. This will also launch the server
-	client.start();
-}
-
-export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
-}
+// Options to control the language client
+const clientOptions: LanguageClientOptions = {
+  // Register the server for plain text documents
+  documentSelector: [{ scheme: "file", language: "plaintext" }],
+  synchronize: {
+    // Notify the server about file changes to '.clientrc files contained in the workspace
+    fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+  },
+};
